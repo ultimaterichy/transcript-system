46484029ee3b0293b1bbd68c4e010580999946c4
Richard Danladi <richarddanladi7@gmail.cpm>
2021-10-14 15:19:08 +0100

    m

--- database/db.config.js
+++ database/db.config.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//     HOST: "remotemysql.com",
+//     USER: "1GsXz1MU4H",
+//     PASSWORD: "WbOhMjxzTs",
+//     DB: "1GsXz1MU4H",
+//     PORT: 3306,
+// };
+
 module.exports = {
-    HOST: "remotemysql.com",
-    USER: "1GsXz1MU4H",
-    PASSWORD: "WbOhMjxzTs",
-    DB: "1GsXz1MU4H",
+    HOST: "www.db4free.net",
+    USER: "wtranscript",
+    PASSWORD: "wtranscript",
+    DB: "wtranscript",
     PORT: 3306,
 };
