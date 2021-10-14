b5a9525d4db9d8f84eddc8b9484c3e02933a2c40
Richard Danladi <richarddanladi7@gmail.cpm>
2021-10-14 10:56:02 +0100

    m

--- database/db.config.js
+++ database/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "transcript"
+    HOST: "sql113.epizy.com",
+    USER: "epiz_30063885",
+    PASSWORD: "081infinity",
+    DB: "epiz_30063885_transcr"
 };
