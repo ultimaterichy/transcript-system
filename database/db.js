148a3e100c8cb985524ef635941b909236829392
Richard Danladi <richarddanladi7@gmail.cpm>
2021-10-28 17:32:04 +0100

    m

--- database/db.js
+++ database/db.js
@@ -12,6 +12,64 @@ const connection = mysql.createConnection({
 // open the MySQL connection
 connection.connect(error => {
   if (error) throw error;
+
+  let students = `
+    CREATE TABLE IF NOT EXISTS students (
+      id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      email varchar(255) NOT NULL,
+      name varchar(255) NOT NULL,
+      phone varchar(20) NOT NULL,
+      matric varchar(255) NOT NULL,
+      department varchar(255) NOT NULL,
+      faculty varchar(255) NOT NULL,
+      public_key varchar(255) NOT NULL,
+      private_key varchar(255) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `;
+
+  let users = `
+    CREATE TABLE IF NOT EXISTS users(
+      id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      email varchar(255) NOT NULL,
+      name varchar(255) NOT NULL,
+      password varchar(255) NOT NULL,
+      role int(1) DEFAULT 0
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `;
+
+  let courses = `
+    CREATE TABLE IF NOT EXISTS \`courses\` (
+      id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      title varchar(255) NOT NULL,
+      code varchar(255) NOT NULL,
+      credit_load int(2) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `;
+
+let add = `
+  insert into courses (title, code, credit_load) values ('Intro to computer 1', 'CMP111', 2), ('Intro to computer 2', 'CMP121', 2), ('Intro to computer 3', 'CMP211', 2), ('Intro to computer 4', 'CMP121', 2), ('Intro to computer 5', 'CMP311', 2);
+`;
+
+  connection.query(students, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  connection.query(users, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  connection.query(courses, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+
+
   console.log("Successfully connected to the database.");
 });
 
